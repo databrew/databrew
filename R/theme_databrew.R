c80405da9fadc27fd9344f0de03b61364ed09892
joebrew <joebrew@gmail.com>
2017-10-01 11:58:24 +0200

    font and functions update

--- R/theme_databrew.R
+++ R/theme_databrew.R
@@ -4,30 +4,36 @@
 
 #' @param base_size The size of the base font
 #' @param base_family The font used in the legend and title
+#' @param y_comma Whether to add comma seperators every 3 digits to a continuous
+#' y axis
 #' @param subtitle_family The font used in the subtitle
 #' @param axis_family The font used in the axes
-#' @return A ggtheme object meant to be used in conjunction with a call to ggplot()
-#' @import RColorBrewer
-#' @import ggplot2
-#' @import extrafont
+#' @return A list of options compatible with \code{ggplot()}
+#' @importFrom grDevices colorRampPalette
+#' @importFrom ggplot2 theme_bw theme element_rect element_line element_blank element_text scale_y_continuous
+#' @importFrom grid unit
+#' @importFrom extrafont loadfonts
+#' @importFrom scales comma
 #' @export
 
 theme_databrew <-
-  function (base_size = 18,
-            base_family = "Roboto",
-            subtitle_family = 'Roboto',
-            axis_family = 'Roboto'){
+  function (base_size = 15,
+            base_family = "Sawasdee",
+            y_comma = TRUE,
+            subtitle_family = 'Sawasdee',
+            axis_family = 'Sawasdee'){
     
     
-    color_background = 'white'#grey(0.99)
+    color_background = '#F8F5E1'#grey(0.99)
     color_grid_major = 'grey'
     color_axis_text =  "#0d63c4"
     color_axis_title = color_axis_text
     color = 'darkgrey'
     color_title = color_axis_text
-    color_subtitle = 'darkgrey'
+    color_subtitle = grey(0.1)
     base_size1 = base_size
-    theme_bw(base_size = base_size1) +
+    out <- 
+      theme_bw(base_size = base_size1) +
       theme(panel.background = element_rect(fill = color_background,
                                             color = color_background)) +
       theme(plot.background = element_rect(fill = color_background,
@@ -45,10 +51,14 @@ theme_databrew <-
                                        color = color_axis_title)) +
       theme(plot.title = element_text(family = base_family,
                                       color = color_title,
-                                      size = base_size * 1.2, vjust = 1.25)) +
+                                      size = base_size * 1.2, 
+                                      # hjust = 4,
+                                      vjust = 1.25)) +
       theme(plot.subtitle = element_text(family = subtitle_family,
                                          color = color_subtitle,
-                                         size = base_size * 0.8, vjust = 1.25)) +
+                                         size = base_size * 0.8, 
+                                         # hjust = -0.95,
+                                         vjust = 1.25)) +
       theme(axis.text.x = element_text(family = axis_family,
                                        size = base_size * 0.7,
                                        color = color_axis_text)) +
@@ -69,4 +79,14 @@ theme_databrew <-
             strip.text = element_text(size = base_size * 0.6),
             panel.spacing = unit(0, "lines"),
             panel.border = element_rect(colour = NA, fill=NA, size=0))
+    
+    # Commas on continous y axes
+    if(y_comma){
+      out <-
+        list(out,
+             scale_y_continuous(label = scales::comma))
+    } else {
+      out <- list(out)
+    }
+    return(out)
   }
